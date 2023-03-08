7829a0536b007eab855dbb6864bb84b1b10c11a5
Allan Valle <allanvalle@outlook.com>
2023-03-08 18:43:40 -0300

    Validações Musician

--- app/models/musician.rb
+++ app/models/musician.rb
@@ -1,3 +1,17 @@
 class Musician < ApplicationRecord
   belongs_to :user
+
+  validates :first_name, :last_name, :nickname, :birth_date, :description, :phone, presence: true
+  validate :permited_age, if: -> { birth_date.present? }
+  validate :permited_phone, if: -> { phone.present? }
+
+  private
+
+  def permited_age
+    errors.add :birth_date, 'Necessário ter mais de 18 anos' if (Date.today - 18.years) < birth_date
+  end
+
+  def permited_phone
+    errors.add :phone, 'Número incorreto (11999999999)' unless phone.match?(/\A\d{11}\z/)
+  end
 end
