6d88fa766f16e2cd490b2007b60bdd5b08344fbe
Allan Valle <allanvalle@outlook.com>
2023-03-09 11:08:28 -0300

    Update routes musician

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -1,2 +1,13 @@
 class MusiciansController < ApplicationController
+  before_action :set_musician, only: %i[show]
+
+  def show
+    authorize(@musician)
+  end
+
+  private
+
+  def set_musician
+    @musician = Musician.find(params[:id])
+  end
 end
