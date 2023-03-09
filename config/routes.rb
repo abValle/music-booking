2610d2456df2761dd6a44390548a1ac7ba28ce06
HURIK ZAMITH <93097561+hurikzamith@users.noreply.github.com>
2023-03-09 16:59:07 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :musicians, except: :destroy
-  resources :companies, except: :index do
-    resources :events, only: %i[new create show]
+  resources :musicians, except: :destroy, do 
+    resource :events, only %i[show index]
+  resources :companies, do
+    resources :events, only: %i[new create show destroy]
   end
-  resources :events, only: :destroy
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
