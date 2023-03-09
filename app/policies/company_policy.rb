870f0546034db04539c1f183f882007ef7958822
hurik zamith <hurikzamith@gmail.com>
2023-03-09 11:13:12 -0300

    seed user e companies criado

--- app/policies/company_policy.rb
+++ app/policies/company_policy.rb
@@ -7,23 +7,23 @@ class CompanyPolicy < ApplicationPolicy
   end
 
   # ----------------------coloco dentro ou fora do scope???´=-----------------------
-  def new?
-    true
-  end
+  # def new?
+  #   true
+  # end
 
-  def create?
-    true
-  end
+  # def create?
+  #   true
+  # end
 
-  def update?
-    # record.user == user
+  # def update?
+  #   # record.user == user
 
-    # record: the restaurant passed to the `authorize` method in controller
-    # user: the `current_user` signed in with Devise
-  end
+  #   # record: the restaurant passed to the `authorize` method in controller
+  #   # user: the `current_user` signed in with Devise
+  # end
 
-  def destroy?
-    # record.user == user
-  end
+  # def destroy?
+  #   # record.user == user
+  # end
   # -------------------------------------------------------------------------------
 end
