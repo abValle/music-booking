af0c302d355d86b0af1223d1cce0b0d8b1026a3a
hurik zamith <hurikzamith@gmail.com>
2023-03-10 14:33:49 -0300

    proposal index

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,92 +31,66 @@ puts 'Creating 10 fake users and companies...'
     description: Faker::Lorem.sentence(word_count: 20),
     user: User.last
   )
+# ---------------------------------------
 
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: "123123",
+    boolean_company: true
+  )
 
+  Company.create!(
+    title: Faker::Company.name,
+    address: Faker::Address.street_address,
+    phone:Faker::Number.number(digits: 11),
+    category: Faker::Music.genre,
+    description: Faker::Lorem.sentence(word_count: 20),
+    user: User.last
+  )
 
-# CRIANDO EVENTS-------------------------
+  2.times do Event.create!(
+    start_date: Date.new(1990, 2, 3),
+    end_date: Date.new(1990, 2, 3),
+    start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+    end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+    price: 300,
+    title_event: Faker::FunnyName.name,
+    description_event: Faker::Lorem.sentence(word_count: 20),
+    company: Company.last
+  )
+  end
+end
 
- 10.times do
+  10.times do
     User.create!(
-    email: Faker::Internet.email,
-    password: "123123",
-    boolean_company: true
-  )
-
-  Company.create!(
-    title: Faker::Company.name,
-    address: Faker::Address.street_address,
-    phone:Faker::Number.number(digits: 11),
-    category: Faker::Music.genre,
-    description: Faker::Lorem.sentence(word_count: 20),
-    user: User.last
-  )
-  Musician.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    nickname: Faker::Books::TheKingkillerChronicle.character,
-    address: Faker::Address.street_address,
-    category: Faker::Music.genre,
-    birth_date: Date.new(1990,2,3),
-    description: Faker::Quotes::Shakespeare.hamlet_quote,
-    phone: Faker::Number.number(digits: 11),
-    user: User.last,
-    rating: Faker::Number.within(range: 1..5)
-  )
-
-    Event.create!(
-      start_date: Date.new(1990, 2, 3),
-      end_date: Date.new(1990, 2, 3),
-      start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-      end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-      price: 300,
-      title_event: Faker::FunnyName.name,
-      description_event: Faker::Lorem.sentence(word_count: 20),
-      company: Company.last
-  )
-    Proposal.create!(
-      musician_id: Musician.last.id,
-      event_id: Event.last.id,
-      winner: nil
+      email: Faker::Internet.email,
+      password: "123123",
+      boolean_company: false
     )
-end
-# --------------------------------------------------------------
+    Musician.create!(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      nickname: Faker::Books::TheKingkillerChronicle.character,
+      address: Faker::Address.street_address,
+      category: Faker::Music.genre,
+      birth_date: Date.new(1990,2,3),
+      description: Faker::Quotes::Shakespeare.hamlet_quote,
+      phone: Faker::Number.number(digits: 11),
+      user: User.last,
+      rating: Faker::Number.within(range: 1..5)
+    )
+  end
 
-10.times do
-  User.create!(
-    email: Faker::Internet.email,
-    password: "123123",
-    boolean_company: true
+  Proposal.create!(
+    musician_id: Musician.last.id,
+    event_id: Event.last.id,
+    winner: nil
   )
 
-  Company.create!(
-    title: Faker::Company.name,
-    address: Faker::Address.street_address,
-    phone:Faker::Number.number(digits: 11),
-    category: Faker::Music.genre,
-    description: Faker::Lorem.sentence(word_count: 20),
-    user: User.last
+  Proposal.create!(
+    musician_id: Musician.first.id,
+    event_id: Event.first.id,
+    winner: nil
   )
-end
-
-10.times do
-  User.create!(
-    email: Faker::Internet.email,
-    password: "123123",
-    boolean_company: false
-  )
-  Musician.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    nickname: Faker::Books::TheKingkillerChronicle.character,
-    address: Faker::Address.street_address,
-    category: Faker::Music.genre,
-    birth_date: Date.new(1990,2,3),
-    description: Faker::Quotes::Shakespeare.hamlet_quote,
-    phone: Faker::Number.number(digits: 11),
-    user: User.last,
-    rating: Faker::Number.within(range: 1..5)
-  )
-end
-
 puts 'Finished!'
