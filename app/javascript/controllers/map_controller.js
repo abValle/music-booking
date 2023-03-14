e04e3af8587960ad7cd2f3526d4d8714ad56c493
Allan Valle <allanvalle@outlook.com>
2023-03-14 12:44:26 -0300

    Adição map em show de event para musician

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -24,6 +24,7 @@ export default class extends Controller {
 
   #addMarkersToMap(){
     this.markersValue.forEach((marker) => {
+      console.log(this.markersValue)
       const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
       new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
