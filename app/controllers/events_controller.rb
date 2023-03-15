aa5ff4432d497c9e4b2ed00a571aec529021a81a
Allan Valle <allanvalle@outlook.com>
2023-03-15 19:42:00 -0300

    Fazer

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -80,4 +80,6 @@ class EventsController < ApplicationController
     end
     map()
   end
+
+  
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,4 @@
 class Event < ApplicationRecord
-
   belongs_to :company
   has_many :proposals, dependent: :destroy
   has_many :musicians, through: :proposals
@@ -8,6 +7,7 @@ class Event < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than: 0, message: "O valor deve ser maior que cero" }
   validates :description_event, presence: true, length: { minimum: 5 }
   validate :end_time_after_start_time
+  validate :overlaps_company_events
 
   include PgSearch::Model
   pg_search_scope :global_search,
@@ -16,7 +16,7 @@ class Event < ApplicationRecord
   # pg_search_scope :price_search, against: [:price], using: { tsearch: { prefix: true } }
 
   CATEGORIES = ['Axé', 'Blues', 'Country', 'Eletrônica', 'Forró', 'Funk', 'Gospel', 'Hip Hop', 'Jazz', 'MPB','Música clássica','Pagode', 'Pop', 'Rap', 'Reggae', 'Rock', 'Samba', 'Sertanejo']
-                  
+
   private
 
   def end_time_after_start_time
@@ -25,5 +25,16 @@ class Event < ApplicationRecord
     if end_time < start_time
       errors.add(:end_time, "Deve ser posterior à data de inicio")
     end
+  end
+
+  def overlaps_company_events
+    events = Event.all
+    incoming_date = (start_time..end_time)
+    result = events.find { |event| incoming_date.overlaps?(event.start_time..event.end_time) && event.company == company }
+
+    raise
+    return unless result
+
+    errors.add(:start_time, "Já existe evento criado para essa data e/ou horário")
   end
 end
