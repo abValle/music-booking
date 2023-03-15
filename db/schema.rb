3a6a4b7ea4c25f0cda9f3a44f5aa36d233f96cb2
hurik zamith <hurikzamith@gmail.com>
2023-03-15 12:05:45 -0300

    migration update

--- db/schema.rb
+++ db/schema.rb
@@ -42,12 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_142657) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "chatrooms", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "companies", force: :cascade do |t|
     t.string "title"
     t.string "address"
