59e1c5c6ca19943bb55786ecc05c06190ce974b6
Allan Valle <allanvalle@outlook.com>
2023-03-15 16:37:56 -0300

    Troca de dado

--- db/migrate/20230315171805_add_category_event_to_events.rb
+++ db/migrate/20230315171805_add_category_event_to_events.rb
@@ -1,5 +1,5 @@
 class AddCategoryEventToEvents < ActiveRecord::Migration[7.0]
   def change
-    add_column :events, :category_event, :string
+    add_column :events, :category_event, :text
   end
 end
