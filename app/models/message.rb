6223ce079fa91786da04c724fead5804d17eb871
hurik zamith <hurikzamith@gmail.com>
2023-03-15 17:41:37 -0300

    notification bell

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,19 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true
+
+  after_create :push_notification
+
+  private
+
+  def push_notification
+    recipient = if self.user == self.proposal.musician.user
+      self.proposal.event.company.user
+    else
+      self.proposal.musician.user
+    end
+
+    push = Push.new(user: recipient, subject: "You have message from #{self.user.nickname}")
+    push.save
+  end
 end
