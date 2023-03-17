768cbdb944b06c8a88d7eaa00e7af3a5b9edd37e
hurik zamith <hurikzamith@gmail.com>
2023-03-17 11:12:22 -0300

    propostas nas paginas de perfil

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,22 +7,30 @@ class PagesController < ApplicationController
 
   def profile_musician
     @musician = current_user.musician
+
     @accepted_proposals = Proposal.where(musician: current_user.musician).and(Proposal.where(winner: true)).includes(:event)
     @refused_proposals = Proposal.where(musician: current_user.musician).and(Proposal.where(winner: false)).includes(:event)
     @pending_proposals = Proposal.where(musician: current_user.musician).and(Proposal.where(winner: nil)).includes(:event)
-    @musician_proposals = policy_scope(Proposal)
-    @musician_proposals = Proposal.all
 
+    @my_proposals = policy_scope(Proposal)
+    @my_proposals = Proposal.all
 
     if current_user.musician
-      @musician_proposals = Proposal.where(musician: current_user.musician)
+      @my_proposals = Proposal.where(musician: current_user.musician)
     else
-      @musician_proposals = Proposal.where(event: current_user.company.events)
+      @my_proposals = Proposal.where(event: current_user.company.events)
     end
   end
 
   def profile_company
+
     @company = current_user.company
     @resource = current_user
+
+    @accepted_proposals = Proposal.where(event_id: current_user.company.events).and(Proposal.where(winner: true)).includes(:event)
+    @refused_proposals = Proposal.where(event_id: current_user.company.events).and(Proposal.where(winner: false)).includes(:event)
+    @pending_proposals =  Proposal.where(event: current_user.company.events).and(Proposal.where(winner: nil)).includes(:event)
+
+    @my_proposals = Proposal.where(event: current_user.company.events).and(Proposal.where(winner: nil)).includes(:event)
   end
 end
