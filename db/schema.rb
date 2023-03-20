e70a50a53d215572cd9e3cec8b63ea092891788f
hurik zamith <hurikzamith@gmail.com>
2023-03-20 11:40:14 -0300

    links redes sociais musicians profile e show prontos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_124919) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_142108) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,7 +91,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_124919) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "instagram_url"
+    t.string "instagram_url"
+    t.string "facebook_url"
+    t.string "spotify_url"
+    t.string "youtube_url"
+    t.string "soundcloud_url"
     t.index ["user_id"], name: "index_musicians_on_user_id"
   end
 
